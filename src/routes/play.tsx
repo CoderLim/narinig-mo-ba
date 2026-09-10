a7521025e2efdc33a3df06ed16efd699c3a59bf2
Lim <779793115@qq.com>
2026-09-10 22:01:34 +0800

    seo: redirect duplicate play page to homepage

---
+++ src/routes/play.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute, redirect } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/play')({
+  beforeLoad: () => {
+    throw redirect({
+      to: '/',
+      statusCode: 301,
+    });
+  },
+});
